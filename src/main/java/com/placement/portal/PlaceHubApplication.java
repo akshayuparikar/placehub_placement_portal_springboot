3094b5e7a9510d3a03f10876c196f2b64493b60e
akshayuparikar <akshayuparikar2002@gmail.com>
2026-05-19 01:04:00 +0530

    Initial commit

---
+++ src/main/java/com/placement/portal/PlaceHubApplication.java
@@ -0,0 +1,11 @@
+package com.placement.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceHubApplication.class, args);
+    }
+}
